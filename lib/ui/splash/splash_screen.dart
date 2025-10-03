45561b80dc30114c433855a31a51d270e076cb73
Julie Ribeiro da Silva <julie@crmsolucoes.com.br>
2025-10-03 15:35:34 -0300

    Loader and messages - SnackBars

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinebox/ui/core/themes/resource.dart';
+import 'package:cinebox/ui/core/widgets/loader_messages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,12 +10,22 @@ class SplashScreen extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends ConsumerState<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> with LoaderAndMessages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Splash Screen'),
+        title: const Text('Splash Screen Riverpod'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(Duration(seconds: 2));
+          hideLoader();
+          showErrorSnackBar("Erro");
+          showSuccessSnackBar("Sucesso");
+          showInfoSnackBar("Info");
+        },
       ),
       body: Center(
         child: Image.asset(R.ASSETS_IMAGES_BG_LOGIN_PNG),
